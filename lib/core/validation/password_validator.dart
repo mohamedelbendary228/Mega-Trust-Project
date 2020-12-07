556dd7554ea1cc2384f45b9613e656caf31c0bc0
Mohamed Elbendary <37798971+mohamedelbendary228@users.noreply.github.com>
2020-12-07 16:28:41 +0200

    Add more impl to bloc and validation

--- lib/core/validation/password_validator.dart
+++ lib/core/validation/password_validator.dart
@@ -1,14 +1,14 @@
-import 'package:auth/core/validation/validator.dart';
-import 'package:injectable/injectable.dart';
-
-@injectable
-class PasswordValidator extends Validator {
-  @override
-  // TODO: implement props
-  List<Object> get props => [];
-
-  @override
-  bool validate(String value) {
-    return value != null && int.parse(value) > 6;
-  }
-}
+//import 'package:auth/core/validation/validator.dart';
+//import 'package:injectable/injectable.dart';
+//
+//@injectable
+//class PasswordValidator extends Validator {
+//  @override
+//  // TODO: implement props
+//  List<Object> get props => [];
+//
+//  @override
+//  bool validate(String value) {
+//    return value != null && int.parse(value) > 6;
+//  }
+//}
--- lib/features/login/presentation/bloc/login_bloc.dart
+++ lib/features/login/presentation/bloc/login_bloc.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'dart:math';
 
 import 'package:auth/core/validation/email_validator.dart';
-import 'package:auth/core/validation/password_validator.dart';
+import 'package:auth/features/login/data/models/login_model.dart';
 import 'package:auth/features/login/data/models/login_response.dart';
 import 'package:auth/features/login/domain/usecases/login_usecases.dart';
 import 'package:bloc/bloc.dart';
@@ -15,18 +15,43 @@ part 'login_state.dart';
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
   final LoginWithEmailAndPasswordUseCases loginWithEmailAndPasswordUseCases;
   final EmailValidator emailValidator;
-  final PasswordValidator passwordValidator;
+
+  //final PasswordValidator passwordValidator;
 
   LoginBloc(
     this.loginWithEmailAndPasswordUseCases,
     this.emailValidator,
-    this.passwordValidator,
+    //this.passwordValidator,
   ) : super(LoginInitial());
 
   @override
   Stream<LoginState> mapEventToState(
     LoginEvent event,
   ) async* {
-    // TODO: implement mapEventToState
+    if (event is EmailTextChangeEvent) {
+      yield ValidEmailState(
+        isValid: emailValidator.validate(event.email),
+        email: event.email,
+      );
+    } else if (event is PasswordTextChangedEvent) {
+      yield ValidPasswordState(isValid: event.password.isNotEmpty);
+    } else if (event is SubmitLoginEvent) {
+      yield* _submitLogin(event);
+    }
+  }
+
+  Stream<LoginState> _submitLogin(SubmitLoginEvent event) async* {
+    final response = await loginWithEmailAndPasswordUseCases.call(
+      LoginParams(
+        loginModel: LoginModel(
+          email: event.email,
+          password: event.password,
+        ),
+      ),
+    );
+    yield response.fold(
+      (l) => FailureLoginState(message: l.message),
+      (r) => SuccessfullyLoginState(r),
+    );
   }
 }
